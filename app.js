6b64b27e5c0ee58e8a167170a4d477c69646a66e
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-03-13 17:53:01 +0100

    milestone 2 push

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-require('./config/db.js');
-console.log('App avviata');
+const express = require('express');
+const app = express();
+const connection = require('./config/db');
+
+app.get('/posts', (req, res) => {
+  const sql = 'SELECT * FROM posts';
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({
+        error: 'Database query non funziona',
+      });
+    }
+
+    res.json(results);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server avviato sulla porta 3000');
+});
